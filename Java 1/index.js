96fe3bc734670d073485cd7d470bb0d7a577f7b4
Islom2023 <124124780+Islom2023@users.noreply.github.com>
2023-02-01 14:10:24 +0500

    Add files via upload

---
+++ Java 1/index.js
@@ -0,0 +1,53 @@
+// let name = " Webbrain Academy";
+// console.log("Welcome " + "to" + name + ", IT CENTER");
+
+// console.log(`Welcome to
+// ${name}, IT Center`);
+
+// console.log(true);
+// console.log(false);
+
+// null
+// let a;
+// let b = null;
+// console.log(typeof a, typeof b);
+// console.log(null === undefined);
+// console.log(null == undefined);
+
+// console.log(null + 1)
+// console.log(undefined + 1)
+
+// ================
+
+// let a;
+// let b = null;
+// console.log(null + 1)
+// console.log(undefined + 1)
+// console.log(a + 1)
+// console.log(b + 1)
+
+// ================
+// Symbols
+// let a = Symbol("text");
+// let b = Symbol("text");
+
+// console.log(a === b); 
+// console.log(a == b); 
+
+// ===============
+// Object - array, object, function
+
+// let array = ['Abdulla', 'Holasak', 'Xursand', 15,] // index
+// console.log(array);
+// console.log(array[0]);
+
+// let object = {name: "Abdulla", kim: "shashlichi"}; // key
+// console.log(object);
+// console.log(object.kim);
+
+let func = ()=>{}
+function test(){
+    console.log(object);
+    console.log(object.kim);
+}
+test()
